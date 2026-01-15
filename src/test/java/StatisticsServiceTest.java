2c9cf3dca4916f96ab693872850d0f2fcc6f226f
Marina Potapova <mp051297@yandex.ru>
2026-01-15 19:52:55 +0300

    JaCoCo commit

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -17,4 +17,15 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void findMaxSubsequence() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        long expected = 10;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
